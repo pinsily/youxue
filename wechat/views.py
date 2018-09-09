78015f83c434281232b6aa9b44f00ffef026e933
pinsily <13160724868@163.com>
2018-09-09 14:18:53 +0800

    update

--- wechat/views.py
+++ wechat/views.py
@@ -1,6 +1,7 @@
 import hashlib
 
-from django.http import HttpResponse
+import six
+from django.http import HttpResponse, HttpResponseForbidden
 from django.shortcuts import render
 
 # Create your views here.
@@ -9,22 +10,40 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def wechat_main(request):
-    if request.method == "GET":
-        # 接收微信服务器get请求发过来的参数
-        signature = request.GET.get('signature', None)
-        timestamp = request.GET.get('timestamp', None)
-        nonce = request.GET.get('nonce', None)
-        echostr = request.GET.get('echostr', None)
-        # 服务器配置中的token
-        token = 'yxuoffernet307943baf3'
-        # 把参数放到list中排序后合成一个字符串，再用sha1加密得到新的字符串与微信发来的signature对比，如果相同就返回echostr给服务器，校验通过
-        hashlist = [token, timestamp, nonce]
-        hashlist.sort()
-        hashstr = ''.join([s for s in hashlist])
-        hashstr = hashlib.sha1(hashstr).hexdigest()
-        if hashstr == signature:
-            return HttpResponse(echostr)
-        else:
-            return HttpResponse("field")
+    timestamp = request.GET.get("timestamp", "")
+    nonce = request.GET.get("nonce", "")
+    signature = request.GET.get("signature", "")
 
-    return HttpResponse("hello")
+    token = "yxuoffernet307943baf3"
+
+    if not check_signature(
+            token=token, timestamp=timestamp, nonce=nonce, signature=signature
+    ):
+        return HttpResponse("hello")
+    if request.method == "GET":
+        return HttpResponse(request.GET.get("echostr", ""))
+
+
+def check_signature(token, timestamp, nonce, signature):
+    if not (token and timestamp and nonce and signature):
+        return False
+    sign = get_signature(token, timestamp, nonce)
+    return sign == signature
+
+
+def get_signature(token, timestamp, nonce, *args):
+    sign = [token, timestamp, nonce] + list(args)
+    sign.sort()
+    sign = to_binary(''.join(sign))
+    return hashlib.sha1(sign).hexdigest()
+
+
+def to_binary(value, encoding="utf-8"):
+    if isinstance(value, six.binary_type):
+        return value
+    if isinstance(value, six.text_type):
+        return value.encode(encoding)
+    return six.binary_type(value)
+
+
+from werobot.contrib.django import make_view
