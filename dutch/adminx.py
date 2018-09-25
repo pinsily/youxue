c1d4e78a026e2e7f0613cc8ff70ad5a8b3d13f51
pinsily <13160724868@163.com>
2018-09-25 20:07:47 +0800

    管理后台

--- dutch/adminx.py
+++ dutch/adminx.py
@@ -2,6 +2,15 @@ from . import models
 import xadmin
 
 
+# 自定义右侧菜单栏
+class GlobalSetting(object):
+    site_title = "行橙拼班"
+    site_footer = '行橙拼班'
+
+
+xadmin.site.register(xadmin.views.CommAdminView, GlobalSetting)
+
+
 class CourseAdmin(object):
     pass
 
@@ -10,5 +19,10 @@ class TeacherAdmin(object):
     pass
 
 
+class ClassUserAdmin(object):
+    pass
+
+
 xadmin.site.register(models.Course, CourseAdmin)
 xadmin.site.register(models.Teacher, TeacherAdmin)
+xadmin.site.register(models.ClassUser, ClassUserAdmin)
